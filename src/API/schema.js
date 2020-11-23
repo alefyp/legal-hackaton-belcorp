fdd1fa95476ca3bfc416b92ba6849e4de64c7a3c
paula113 <paulapierina96@gmail.com>
2020-11-23 18:48:42 -0500

    data schema

---
+++ src/API/schema.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        name: '',
+        description: '',
+        startingDate: '',
+        owner: '',
+        countries: [],
+        recomendations: [
+            {
+                title: '',
+                date: '',
+                content: '',
+            }
+        ],
+        risks: [
+            {
+                type: '',
+                level: ''
+            }
+        ],
+        Attachments: []
+    }
+]
+// riskLevel: ['Alto', 'medio', 'bajo']
+//   risksTypes: [ 'Riesgo Laboral', 
+//         'Riesgo Reputacional', 
+//         'Riesgo Financiero', 
+//         'Riesgo Tributario', 
+//         'Riesgo de Propiedad Intelectual', 
+//         'Riesgo Ambiental', 
+//         'Riesgo Publicitario',
+//         'Riesgo de Consumidor',
+//         'Riesgo Seguridad de la Información',
+//         'Riesgo de Libre Competencia'
+//         ]
