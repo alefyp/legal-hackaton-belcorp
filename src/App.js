eb9b220b783dd8866ad4f43c97701ea884c089fc
paula113 <paulapierina96@gmail.com>
2020-11-24 11:47:00 -0500

    slint running

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, {useState } from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import Login from './views/login'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './views/login';
 import Dashboard from './views/dashboard';
 
-
-  const App = () => {
-    const [user, setUser]= useState('');
+const App = () => {
+  const [user, setUser] = useState('');
 
   return (
     <div className="App">
-     
+
       <BrowserRouter>
         <div>
           <Switch>
-            <Route exact path='/' > <Login user ={user} setUser={setUser}/></Route>{/* Public Page */}
-            <Route path='/dashboard' > <Dashboard /></Route> {/* //already login */}
+            <Route exact path="/">
+              {' '}
+              <Login user={user} setUser={setUser} />
+            </Route>
+            {/* Public Page */}
+            <Route path="/dashboard">
+              {' '}
+              <Dashboard />
+            </Route>
+            {' '}
+            {/* //already login */}
           </Switch>
         </div>
-        </BrowserRouter>
-    
+      </BrowserRouter>
+
     </div>
   );
 };
-
 
 export default App;
