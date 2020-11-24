328dd5d8f3a2ec51a049b7617a4c290c6b629a9e
paula113 <paulapierina96@gmail.com>
2020-11-24 12:02:16 -0500

    eslint running wth microsoft authentication

--- src/views/login.js
+++ src/views/login.js
@@ -1,53 +1,52 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import auth from '../firebaseInit.js'; 
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
+import auth from '../firebaseInit';
 
-import './login.css'
+import './login.css';
 
-export default function Login ({history, user, setUser}) {
-
-    const provider = new firebase.auth.OAuthProvider('microsoft.com');
-    provider.addScope('User.Read');
+export default function Login({ history, user, setUser }) {
+  const provider = new firebase.auth.OAuthProvider('microsoft.com');
+  provider.addScope('User.Read');
 
   const funcionAlgo = (prov) => {
-    
-      console.log(provider);
-      return    auth.signInWithPopup(prov)
-         
-        
-        .then(function(result) {
-          console.log('realmente ke está pazanda');
-          console.log(result);
-          console.log(result.additionalUserInfo.profile, result.credential.idToken);
-          console.log(firebase.auth.UserCredential);
-          // User is signed in.
-          // IdP data available in result.additionalUserInfo.profile.
-          // OAuth access token can also be retrieved:
-          // result.credential.accessToken
-          // OAuth ID token can also be retrieved:
-          // result.credential.idToken
-        })
-        .catch(function(error) {
-            console.log(error)
-          // Handle error.
-        });
-    }
-   
+    console.log(provider);
+    return auth.signInWithPopup(prov)
 
-console.log(user)
-    return (
-      <>
-          <form className='login-form'>
-        <label>Usuario:
-            <input className='user-name'onBlur={(e)=>setUser(e.target.value)}></input>
+      .then((result) => {
+        console.log('realmente ke está pazanda');
+        console.log(result);
+        console.log(result.additionalUserInfo.profile, result.credential.idToken);
+        console.log(firebase.auth.UserCredential);
+        // User is signed in.
+        // IdP data available in result.additionalUserInfo.profile.
+        // OAuth access token can also be retrieved:
+        // result.credential.accessToken
+        // OAuth ID token can also be retrieved:
+        // result.credential.idToken
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle error.
+      });
+  };
+
+  console.log(user);
+  return (
+    <>
+      <form className="login-form">
+        <label htmlFor="user-name">
+          Usuario:
+          <input className="user-name" onBlur={(e) => setUser(e.target.value)} />
         </label>
-        
-        <label>Contraseña:
-            <input className='user-password'></input>
+
+        <label htmlFor="user-password">
+          Contraseña:
+          <input className="user-password" />
         </label>
-        <button onClick={() => history.push('/dashboard')}>Log in</button>
-        <button onClick={()=> funcionAlgo(provider)}>Outlook</button>
-        </form>
-      </>
-    );
+        <button type="button" onClick={() => history.push('/dashboard')}>Log in</button>
+        <button type="button" onClick={() => funcionAlgo(provider)}>Outlook</button>
+      </form>
+    </>
+  );
 }
